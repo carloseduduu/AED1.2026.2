6bb87f0902eec07bc9f7854a934c31b0dd00ca1c
carloseduduu <carlosfreireedu08@gmail.com>
2026-09-11 17:24:25 -0300

    Rename

--- exerc_BOCA/boca2/Custo da Lata de Cerveja (+).c
+++ exerc_BOCA/boca2/Custo da Lata de Cerveja (+).c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+#define PI 3.14159
+
+int main() {
+    
+    float area, r, h;
+
+    scanf("%f %f", &r, &h);
+
+    area = 2.0*(PI*r*r)+(2.0*PI*r*h);
+
+    printf("O VALOR DO CUSTO E = %.2f", area*100.00);
+
+    return 0;
+}
